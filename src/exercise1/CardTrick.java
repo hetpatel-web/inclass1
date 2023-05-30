53ddea1968f06e0b90b7f0e632ba831adf1a1157
Het Patel <hetpatel.web@gmail.com>
2023-05-30 09:54:36 -0400

    Update on GitHub ex1 branch

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -88,16 +88,16 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
 
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Het, but you can call me prof, Paul or sir");
         System.out.println();
 
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
+        System.out.println("-- Be a software Dev");
         System.out.println("-- Have a semester with no violations of academic integrity!");
         System.out.println();
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
+        System.out.println("-- 3D printing");
         System.out.println("-- Cooking");
         System.out.println("-- Reading/Watching TV");
         System.out.println("-- Riding my motorcycle");
