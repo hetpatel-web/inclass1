184230f33121f395ec3b68c4b4c36524f850eb2b
Het Patel <hetpatel.web@gmail.com>
2023-05-30 09:59:44 -0400

    I am Done!

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -84,7 +84,7 @@ public class CardTrick {
      * @author Paul Bonenfant Jan 2022
      */
     private static void printInfo() {
-
+        // I am Done
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
 
